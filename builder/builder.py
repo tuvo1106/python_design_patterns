f7d6a162ae50db8152cee6fcfd4d4b79babd7f45
Tu Vo <tuvo1106@gmail.com>
2019-07-27 15:55:26 -0700

    Add builder projects

---
+++ builder/builder.py
@@ -0,0 +1,104 @@
+"""
+Builder
+ - When piecewise object construction is complicated,
+   provide an API for doing it succintly
+
+Motivation
+- some objects are simply and can be created in a single
+  initializer call
+- other objects require a lot of ceremony to create
+- having an object with 10 initializer arguments is not
+  productive
+- instead, opt for piecewise construction
+- Builder provides an API for constructing an object
+  step-by-step
+
+"""
+
+# simple scenario
+text = 'hello'
+parts = ['<p>', text, '</p>']
+print(''.join(parts))
+
+# more complicated
+words = ['hello', 'world']
+parts = ['<ul>']
+for w in words:
+    parts.append(f'\t<li>{w}</li>')
+parts.append('</ul>')
+print('\n'.join(parts))
+
+class HtmlElement:
+    indent_size = 2
+
+    def __init__(self, name="", text=""):
+        self.name = name
+        self.text = text
+        self.elements = []
+
+    def __str(self, indent):
+        lines = []
+        i = ' ' * (indent * self.indent_size)
+        lines.append(f'{i}<{self.name}>')
+
+        if self.text:
+            i1 = ' ' * ((indent + 1) * self.indent_size)
+            lines.append(f'{i1}{self.text}')
+
+        for e in self.elements:
+            lines.append(e.__str(indent + 1))
+
+        lines.append(f'{i}</{self.name}>')
+        return '\n'.join(lines)
+
+    def __str__(self):
+        return self.__str(0)
+
+    @staticmethod
+    def create(name):
+        return HtmlBuilder(name)
+
+
+class HtmlBuilder:
+    __root = HtmlElement()
+
+    def __init__(self, root_name):
+        self.root_name = root_name
+        self.__root.name = root_name
+
+    # not fluent
+    def add_child(self, child_name, child_text):
+        self.__root.elements.append(
+            HtmlElement(child_name, child_text)
+        )
+
+    # fluent
+    def add_child_fluent(self, child_name, child_text):
+        self.__root.elements.append(
+            HtmlElement(child_name, child_text)
+        )
+        return self
+
+    def clear(self):
+        self.__root = HtmlElement(name=self.root_name)
+
+    def __str__(self):
+        return str(self.__root)
+
+builder = HtmlBuilder('ul')
+builder.add_child('li', 'hello')
+builder.add_child('li', 'world')
+print('Ordinary builder:')
+print(builder)
+
+# chaining methods
+fluent_builder = HtmlBuilder('ul')
+fluent_builder.add_child_fluent('li', 'hello').add_child_fluent('li', 'world')
+print('Fluent builder:')
+print(fluent_builder)
+
+# create
+created_builder = HtmlElement.create('ul')
+created_builder.add_child_fluent('li', 'python')
+print('Created builder:')
+print(created_builder)
---
+++ builder/builder_facets.py
@@ -0,0 +1,75 @@
+class Person:
+    def __init__(self):
+        # address
+        self.street_address = None
+        self.postcode = None
+        self.city = None
+
+        # employment
+        self.company_name = None
+        self.position = None
+        self.annual_income = None
+
+    def __str__(self):
+        return f'Address: {self.street_address}, {self.postcode}, {self.city}' + \
+        f'\nEmployed at {self.company_name} as a {self.position} earning {self.annual_income}'
+
+class PersonBuilder:
+    def __init__(self, person=Person()):
+        self.person = person
+
+    @property
+    def works(self):
+        return PersonJobBuilder(self.person)
+
+    @property
+    def lives(self):
+        return PersonAddressBuilder(self.person)
+
+    def build(self):
+        return self.person
+
+class PersonJobBuilder(PersonBuilder):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def at(self, company_name):
+        self.person.company_name = company_name
+        return self
+
+    def as_a(self, position):
+        self.person.position = position
+        return self
+
+    def earning(self, annual_income):
+        self.person.annual_income = annual_income
+        return self
+
+class PersonAddressBuilder(PersonBuilder):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def at(self, street_address):
+        self.person.street_address = street_address
+        return self
+
+    def at_postcode(self, postcode):
+        self.person.postcode = postcode
+        return self
+
+    def in_city(self, city):
+        self.person.city = city
+        return self
+
+pb = PersonBuilder()
+person = pb\
+    .lives\
+        .at('123 London Road')\
+        .in_city('London')\
+        .at_postcode('SW12BC')\
+    .works\
+        .at('Fabrikam')\
+        .as_a('Engineer')\
+        .earning(123000)\
+    .build()
+print(person)
